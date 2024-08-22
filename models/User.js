1d21923b633c4c8ad696ac7473b01a0cd6989d93
siMarzouki <marzouki.mohamedaziz@esprit.tn>
2024-08-22 22:38:45 +0200

    feat: unable password select

--- models/User.js
+++ models/User.js
@@ -23,7 +23,6 @@ const userSchema = new Schema({
     type: String,
     required: true,
     minlength: 6,
-    select: false, // To exclude password from query results by default
   },
   roles: {
     type: Object,
