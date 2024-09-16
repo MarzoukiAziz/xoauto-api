9df1b7c2c727bd68e7791f48c06204c1f483355a
MarzoukiAziz <marzouki.mohamedaziz@esprit.tn>
2024-09-16 02:39:21 +0100

    fix: correct comment routes

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const { createComment, updateComment, deleteComment, getCommentsByArticleId } = require('../controllers/commentController');
 const verifyUserRoles = require("../middlewares/verifyUserRoles")
 const ROLES_LIST = require("../utils/rolesList")
+const verifyUserToken = require("../middlewares/verifyUserToken")
 
 router.get('/article/:articleId', getCommentsByArticleId);
-router.post('/', verifyUserRoles(ROLES_LIST.USER), createComment);
-router.put('/:id', verifyUserRoles(ROLES_LIST.USER), updateComment);
-router.delete('/:id', verifyUserRoles(ROLES_LIST.ADMIN, ROLES_LIST.USER), deleteComment);
+router.post('/', verifyUserToken, verifyUserRoles(ROLES_LIST.USER), createComment);
+router.put('/:id', verifyUserToken, verifyUserRoles(ROLES_LIST.USER), updateComment);
+router.delete('/:id', verifyUserToken, verifyUserRoles(ROLES_LIST.ADMIN, ROLES_LIST.USER), deleteComment);
 
 module.exports = router;
