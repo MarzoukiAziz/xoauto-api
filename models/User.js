ae36dd29f414190110e4265251725d7dc459d12a
MarzoukiAziz <marzouki.mohamedaziz@esprit.tn>
2024-09-27 20:20:02 +0100

    feat: udpate user model

--- models/User.js
+++ models/User.js
@@ -1,7 +1,4 @@
 const mongoose = require("mongoose");
-const crypto = require("crypto");
-const softDeleteMiddleware = require("../middlewares/softDeleteMiddleware");
-
 const { Schema } = mongoose;
 
 const userSchema = new Schema(
@@ -23,34 +20,14 @@ const userSchema = new Schema(
         "Please enter a valid email",
       ],
     },
-    phone: { type: String, required: false },
     avatar: { type: String, required: false },
     pro: { type: Boolean, required: false },
-    favoris: [{ type: String }],
+    favorite_ads: [{ type: String }],
     lastLogin: { type: Date, default: null },
   },
   {
     timestamps: true, // Add createdAt and updatedAt fields
   }
 );
-
-// Apply the softDeleteMiddleware to the schema
-userSchema.plugin(softDeleteMiddleware);
-
-const tokenExpirationMinutes = parseInt(
-  process.env.PASSWORD_RESET_TOKEN_EXPIRATION,
-  10
-);
-
-// Generate password reset token
-userSchema.methods.generatePasswordResetToken = function () {
-  const resetToken = crypto.randomBytes(20).toString("hex");
-  this.passwordResetToken = crypto
-    .createHash("sha256")
-    .update(resetToken)
-    .digest("hex");
-  this.passwordResetExpires = Date.now() + tokenExpirationMinutes * 60 * 1000; // Token expires in 30 minutes
-  return resetToken;
-};
 
 module.exports = new mongoose.model("User", userSchema);
