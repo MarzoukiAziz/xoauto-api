0cc498d022af3b6256602e5a7bf16ef47f30001b
MarzoukiAziz <marzouki.mohamedaziz@esprit.tn>
2024-10-09 22:47:52 +0100

    feat: add new settings service

--- controllers/settings/settingsController.js
+++ controllers/settings/settingsController.js
@@ -5,7 +5,11 @@ const Category = require("../../models/settings/Category");
 const Energy = require("../../models/settings/Energy");
 const Region = require("../../models/settings/Region");
 const Equipment = require("../../models/settings/Equipment");
+const NewSettings = require("../../models/settings/NewSettings");
 
+/***********************
+ * USED
+ ****************/
 // Get Settings
 const getSettings = async (req, res, next) => {
   try {
@@ -32,6 +36,60 @@ const getSettings = async (req, res, next) => {
   }
 };
 
+/***********************
+ * NEW
+ ****************/
+
+const getNewSettingsWithBrands = async (req, res, next) => {
+  try {
+    const newSettings = await NewSettings.findOne();
+    if (!newSettings) {
+      return res.status(404).json({ message: "No settings found" });
+    }
+    const availableBrands = await Brand.find({
+      _id: { $in: newSettings.brands },
+    }).sort({ name: 1 });
+
+    res.status(200).json({ brands: availableBrands });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getNewSettings = async (req, res, next) => {
+  try {
+    const newSettings = await NewSettings.findOne();
+    if (!newSettings) {
+      return res.status(404).json({ message: "No settings found" });
+    }
+
+    res.status(200).json(newSettings);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateNewSettings = async (req, res, next) => {
+  const updateData = req.body;
+
+  try {
+    const updatedSettings = await NewSettings.findOneAndUpdate({}, updateData, {
+      new: true,
+    });
+
+    if (!updatedSettings) {
+      return res.status(404).json({ message: "No settings found to update" });
+    }
+
+    res.status(200).json(updatedSettings);
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   getSettings,
+  getNewSettingsWithBrands,
+  getNewSettings,
+  updateNewSettings,
 };
--- routes/settings/settingRoutes.js
+++ routes/settings/settingRoutes.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const {
   getSettings,
+  getNewSettings,
+  getNewSettingsWithBrands,
+  updateNewSettings,
 } = require("../../controllers/settings/settingsController");
+const verifyUserToken = require("../../middlewares/verifyUserToken");
+const verifyUserRoles = require("../../middlewares/verifyUserRoles");
+const ROLES_LIST = require("../../utils/rolesList");
 
 const router = express.Router();
 
 router.get("/", getSettings);
+router.get("/new", getNewSettings);
+router.get("/new-details", getNewSettingsWithBrands);
+
+router.put(
+  "/new",
+  verifyUserToken,
+  verifyUserRoles(ROLES_LIST.ADMIN),
+  updateNewSettings
+);
 
 module.exports = router;
