c861f7c1ad1e67e0828e818aa2423d56fff7da2d
MarzoukiAziz <marzouki.mohamedaziz@esprit.tn>
2024-10-14 04:29:31 +0100

    feat: communicator between microservices

--- communicator/index.js
+++ communicator/index.js
@@ -3,20 +3,20 @@ const axios = require("axios");
 class Communicator {
   constructor() {
     this.userServiceClient = axios.create({
-      baseURL: "http://localhost:5001/api",
+      baseURL: "http://localhost:5001/api/v1",
     });
     this.adServiceClient = axios.create({
-      baseURL: "http://localhost:5002/api",
+      baseURL: "http://localhost:5002/api/v1",
     });
     this.newAdServiceClient = axios.create({
-      baseURL: "http://localhost:5003/api",
+      baseURL: "http://localhost:5003/api/v1",
     });
     this.blogServiceClient = axios.create({
-      baseURL: "http://localhost:5004/api",
+      baseURL: "http://localhost:5004/api/v1",
     });
   }
-  async getUsers() {
-    const response = await this.userServiceClient.get("/users");
+  async getSavedAds(uid) {
+    const response = await this.userServiceClient.get("/user/saved/" + uid);
     return response.data;
   }
 }
