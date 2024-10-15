a2c2ce5e714d61e7b296c5417b46a4b3c8db9694
MarzoukiAziz <marzouki.mohamedaziz@esprit.tn>
2024-10-15 22:31:32 +0100

    feat: refactor color services

--- ad-service/controllers/settings/colorController.js
+++ ad-service/controllers/settings/colorController.js
@@ -1,32 +1,31 @@
-const Color = require("../../models/settings/Color");
+const colorService = require("../../services/colorService");
 
 // Get all Colors
 const getColors = async (req, res, next) => {
   try {
-    const colors = await Color.find().sort({ name_fr: 1 });
+    const colors = await colorService.getAllColors();
     res.status(200).json(colors);
   } catch (error) {
     next(error);
   }
 };
 
-// Create a new color
+// Create a new Color
 const createColor = async (req, res, next) => {
   try {
-    const color = new Color(req.body);
-    await color.save();
+    const color = await colorService.createNewColor(req.body);
     res.status(201).json(color);
   } catch (error) {
     next(error);
   }
 };
 
-// Delete a color by ID
+// Delete a Color by ID
 const deleteColor = async (req, res, next) => {
   const { id } = req.params;
 
   try {
-    const deletedColor = await Color.findByIdAndRemove(id);
+    const deletedColor = await colorService.deleteColorById(id);
     if (!deletedColor) {
       return res.status(404).json({ message: "Color not found" });
     }
